84f5af5ce6a90f97bded1adf701edc7fc1519ff0
cwherring <chris@the-herring.net>
2024-12-07 16:35:06 -0600

    Added circleahape class

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ def main():
         event = pygame.event.poll()
         if event.type == pygame.QUIT:
             running = False
-        screen.fill((0,0,0))
+        screen.fill("black")
         pygame.display.flip()
         dt = clock.tick(60)/1000
-        
+
 
 if __name__ == "__main__":
     main()
