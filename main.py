b9772c75c7348fe3fb7c8fd8c3ae6e9bfd431d8a
cwherring <chris@the-herring.net>
2024-12-08 18:49:10 -0600

    Added player.update, .move, and .rotate

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
 from constants import *
+
 
 def main():
     print("Starting asteroids!")
@@ -12,12 +14,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    player = Player(x,y)
     running = True
     while running:
         event = pygame.event.poll()
         if event.type == pygame.QUIT:
             running = False
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
