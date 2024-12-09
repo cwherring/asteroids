cf1aea1d2fe609240aaf3a585b2f3077e1a85653
cwherring <chris@the-herring.net>
2024-12-09 16:18:29 -0600

    Added shooting with shot delay

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 from player import Player
 from asteroid import Asteroid
+from shot import Shot
 from asteroidfield import AsteroidField
 from constants import *
 
@@ -17,9 +18,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
     dt = 0
     x = SCREEN_WIDTH/2
     y = SCREEN_HEIGHT/2
--- player.py
+++ player.py
@@ -1,10 +1,12 @@
 from circleshape import *
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
     # in the player class
     def triangle(self):
@@ -32,9 +34,21 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
-
+        if keys[pygame.K_SPACE]:
+            if self.timer <= 0:
+                self.timer = PLAYER_SHOOT_COOLDOWN
+                self.shoot()
+                
+        self.timer -= dt
+ 
     def move(self, delta):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * delta       
+        self.position += forward * PLAYER_SPEED * delta 
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        #shot.position += shot.velocity * delta 
+        
 
 
