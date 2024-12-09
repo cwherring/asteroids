8e6fdd20c8afabe9ccb9a899e46fcb4c6ac7703d
cwherring <chris@the-herring.net>
2024-12-08 18:57:40 -0600

    Added groups and updated game loop to use them

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     dt = 0
     x = SCREEN_WIDTH/2
     y = SCREEN_HEIGHT/2
@@ -23,8 +26,10 @@ def main():
         if event.type == pygame.QUIT:
             running = False
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for thing in updateable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
