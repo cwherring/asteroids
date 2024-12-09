c9a87f92a21bf7963efd69007b20b2674691e924
cwherring <chris@the-herring.net>
2024-12-08 19:30:21 -0600

    minor updates to flow

--- asteroid.py
+++ asteroid.py
@@ -4,8 +4,7 @@ from constants import *
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-        self.position = pygame.Vector2(x, y)
-
+ 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
--- main.py
+++ main.py
@@ -24,17 +24,21 @@ def main():
     x = SCREEN_WIDTH/2
     y = SCREEN_HEIGHT/2
     player = Player(x,y)
-    asteriodfield = AsteroidField()
+    asteriod_field = AsteroidField()
     running = True
     while running:
         event = pygame.event.poll()
         if event.type == pygame.QUIT:
             running = False
-        screen.fill("black")
+
         for thing in updatable:
             thing.update(dt)
+
+        screen.fill("black")
+
         for thing in drawable:
             thing.draw(screen)
+            
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
